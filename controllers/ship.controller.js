0b5dfdea197c6315eededa237717eea57bb5110a
arun kumar <arun.devpro@gmail.com>
2024-09-26 22:04:20 +0530

    create shipment

--- controllers/ship.controller.js
+++ controllers/ship.controller.js
@@ -1,4 +1,5 @@
 import Ship from "../models/ship.models";
+import Shipment from "../models/shipment.models";
 import ErrorHandler from "../utils/error.handler";
 import SendResponse from "../utils/responseHandler";
 
@@ -16,4 +17,35 @@ export const onBoardShip = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+};
+
+export const createShipment = async (req, res, next) => {
+  const {
+    startCoordinate,
+    endCoordinate,
+    shipmentStatus = "assigned",
+    shipId,
+  } = req.body;
+
+  if (!startCoordinate || !endCoordinate)
+    return ErrorHandler(res, 400, "Provide start and end coordinates");
+
+  const ship = await Ship.findById(shipId).lena();
+  if (!ship) return ErrorHandler(res, 400, "Ship not found");
+
+  const shipment = await Shipment.create({
+    shipId: ship._id,
+    "startCoordinate.coordinates": startCoordinate,
+    "endCoordinate.coordinates": endCoordinate,
+    shipmentStatus,
+  });
+
+  await Ship.findById(
+    shipId,
+    { $addToSet: { shipments: shipment._id } },
+    { new: true, upsert: true }
+  );
+
+  // return response
+  return SendResponse(res, 201, `Shipment assigned for ${ship.shipName}`);
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-import express from 'express';
+import express from "express";
+import { createShipment, onBoardShip } from "../controllers/ship.controller";
 const router = express.Router();
 
 // define routes
 router.route("/onboard-ship").post(onBoardShip);
-
+router.route("/create-shipment").post(createShipment);
 
 export default router;
