2d50dc7f84d7beedcde157be11b29f01c53d841f
arun kumar <arun.devpro@gmail.com>
2024-09-26 20:01:59 +0530

    server started

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+
+const { port = 8000 } = process.env.PORT;
+
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
