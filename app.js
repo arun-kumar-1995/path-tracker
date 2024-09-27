fbbd3a1a19d3795b489d9177725eb254bfbc1c44
arun kumar <arun.devpro@gmail.com>
2024-09-27 17:07:46 +0530

    generic api handler

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 import express from "express";
 const app = express();
+import cors from "cors";
 
+// Use CORS middleware
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 export default app;
---
+++ frontend/src/services/index.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// Create an instance of Axios with base configuration
+const api = axios.create({
+  baseURL: process.env.API_URL, 
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Generic GET request
+export const get = (url, config = {}) => {
+  return api.get(url, config);
+};
+
+// Generic POST request
+export const post = (url, data, config = {}) => {
+  return api.post(url, data, config);
+};
+
+// Generic PUT request
+export const put = (url, data, config = {}) => {
+  return api.put(url, data, config);
+};
+
+// Generic DELETE request
+export const del = (url, config = {}) => {
+  return api.delete(url, config);
+};
+
+export default api;
